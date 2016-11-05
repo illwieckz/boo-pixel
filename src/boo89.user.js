cba2d3fd723d14fa216055b0476a035d9ca1c2cf
Thomas Debesse <dev@illwieckz.net>
2016-11-06 00:15:12 +0100

    fix sleeping animation
    - let GreaseMonkey run the script at document-end (do not add an event listener)
    - add explicit @run at document-end even if it's the current default, because we know we need it
    - name the resources with .png extension so the original script from the website itself can find the animation frames based on their names

--- src/boo89.user.js
+++ src/boo89.user.js
@@ -7,43 +7,31 @@
 // @version           1
 // @include           /^https?://www\.yaronet\.com/.*$/
 // @icon              boo89.png
-// @resource          boo89          boo89.png
-// @resource          boo89-sleep    boo89-sleep.png
-// @resource          boo89-sleep-z0 boo89-sleep-z0.png
-// @resource          boo89-sleep-z1 boo89-sleep-z1.png
-// @resource          boo89-sleep-z2 boo89-sleep-z2.png
+// @resource          boo89.png          boo89.png
+// @resource          boo89-sleep.png    boo89-sleep.png
+// @resource          boo89-sleep-z0.png boo89-sleep-z0.png
+// @resource          boo89-sleep-z1.png boo89-sleep-z1.png
+// @resource          boo89-sleep-z2.png boo89-sleep-z2.png
+// @run-at            document-end
 // @grant             GM_getResourceURL
 // ==/UserScript==
 
-console.log("boo89:load");
-document.addEventListener("DOMContentLoaded", function(event) {
-	console.log("boo89:run");
-	var elemBoo = document.getElementById('yn-boo')
-	var currentBoo = elemBoo.getAttribute('src').split('/').pop();
-	console.log(currentBoo);
-	if (currentBoo) {
-		console.log("boo89:if-split");
-		if (currentBoo === 'default.png') {
-			console.log("boo89:if-default");
-			console.log(GM_getResourceURL('boo89'));
-			elemBoo.setAttribute('src', GM_getResourceURL('boo89'));
-		}
-		else if (currentBoo === 'sleep.png') {
-			console.log("boo89:if-sleep");
-			changeNextSiblings(elemBoo, "z0");
-			changeNextSiblings(elemBoo, "z1");
-			changeNextSiblings(elemBoo, "z2");
-			elemBoo.setAttribute('src', GM_getResourceURL('boo89-sleep'));
-		}
-	}
+// The sleep-z#.png images are loaded by an already existing script (…/layout/html/page.js) from
+// the website itself that runs after the GreaseMonkey scripts, computing path from existing image
+// path if it ends with “.png” so there is no need to care about them. Also, since these images
+// are loaded by a script at run-time, we can't substitute them when DOMContentLoaded occurs. It's
+// better to let the original script doing it after the GreaseMonkey script has run, that's why we
+// run it on “document-end”, feeding the original script with what it looks for. That's also why
+// we named resources with file extensions even if GreaseMonkey does not require them.
 
-	function changeNextSiblings(elemBoo, zzz) {
-		var list = elemBoo.getElementsByClassName("." + zzz);
-		for (var i = 0; i < list.length; i++) {
-			var el = list[i];
-			while (el = el.nextSibling) {
-				el.setAttribute('src', GM_getResourceURL('boo89-sleep-' + zzz));
-			}
-		}
+var elemBoo = document.getElementById('yn-boo');
+var currentBoo = elemBoo.getAttribute('src').split('/').pop();
+if (currentBoo) {
+	if (currentBoo === 'default.png') {
+		console.log(GM_getResourceURL('boo89'));
+		elemBoo.setAttribute('src', GM_getResourceURL('boo89.png'));
 	}
-});
+	else if (currentBoo === 'sleep.png') {
+		elemBoo.setAttribute('src', GM_getResourceURL('boo89-sleep.png'));
+	}
+}
